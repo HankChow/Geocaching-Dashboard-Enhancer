075ab6314a3d04c98cc8bf813eab099c549270f7
HankChow <280630620@qq.com>
2025-04-14 22:36:09 +0800

    feat: add show/hide all logs

--- content.js
+++ content.js
@@ -209,6 +209,7 @@ async function fetchGeocacheDetails(gcCodes, prCode) {
           link.insertAdjacentElement('afterend', metaData);          
         });
       }
+      checkFinishedFounderDateSummaryMetadata();
     } catch (error) {
       console.error(`Error fetching details for gcCode ${gcCode}:`, error);
     }
@@ -439,6 +440,49 @@ function getNearestDate() {
   });
 }
 
+function checkFinishedFounderDateSummaryMetadata() {
+  document.querySelectorAll('details.full-list-of-found-caches').forEach(details => {
+    if (!details.getAttribute('data-loading-finished')) {
+      const pElements = details.querySelectorAll(':scope > p');
+      const metadataElements = details.querySelectorAll(':scope > p span.cache-metadata');
+      if (pElements.length === metadataElements.length) {
+        const toggleAllLogsSpan = details.querySelector('span.all-logs-toggle');
+        const showAllLogsLink = document.createElement('a');
+        showAllLogsLink.href = '#';
+        showAllLogsLink.textContent = '[Show All Logs]';
+        const hideAllLogsLink = document.createElement('a');
+        hideAllLogsLink.href = '#';
+        hideAllLogsLink.textContent = '[Hide All Logs]';
+        toggleAllLogsSpan.appendChild(showAllLogsLink);
+        toggleAllLogsSpan.appendChild(hideAllLogsLink);
+        if (!details.open) {
+          toggleAllLogsSpan.hidden = true;
+        }
+
+        showAllLogsLink.onclick = async function(e) {
+          e.preventDefault();
+          this.parentElement.parentElement.parentElement.querySelectorAll('a.metadata-toggle').forEach(item => {
+            if (item.textContent == '[Show Log]') {
+              item.click();
+            }
+          })
+        };
+
+        hideAllLogsLink.onclick = async function(e) {
+          e.preventDefault();
+          this.parentElement.parentElement.parentElement.querySelectorAll('a.metadata-toggle').forEach(item => {
+            if (item.textContent == '[Hide]') {
+              item.click();
+            }
+          })
+        };
+
+        details.setAttribute('data-loading-finished', true);
+      }
+    }
+  })
+}
+
 // Main logic
 async function loadFullFoundCacheList() {
   log('Main starts.');
@@ -502,9 +546,20 @@ async function loadFullFoundCacheList() {
               const details = document.createElement('details');
               details.className = 'full-list-of-found-caches';
               details.setAttribute('data-founder-name', username);
+              details.setAttribute('data-found-date', activityGroupDay);
 
               const summary = document.createElement('summary');
-              summary.textContent = 'Full List of Found Caches';
+              summary.textContent = 'Full List of Found Caches';              
+              const toggleAllLogsSpan = document.createElement('span');
+              toggleAllLogsSpan.className = 'all-logs-toggle';
+              summary.insertAdjacentElement('beforeend', toggleAllLogsSpan);
+              details.addEventListener('toggle', function() {
+                if (this.open) {
+                  this.querySelector('span.all-logs-toggle').hidden = false;
+                } else {
+                  this.querySelector('span.all-logs-toggle').hidden = true;
+                }
+              });
               details.appendChild(summary);
               details.appendChild(foundCachesList);
               activityDetails.appendChild(details);
@@ -569,5 +624,5 @@ async function loadRecentNearbyActivities() {
   log('loadRecentNearbyActivities finishes.');
 }
 
-// loadFullFoundCacheList();
-loadRecentNearbyActivities();
+loadFullFoundCacheList();
+// loadRecentNearbyActivities();
